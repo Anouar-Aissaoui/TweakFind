321a500e2012912077f2d20012f787437808d106
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-12 16:49:43 +0000

    pls optimize : seo categories list

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,12 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { apps } from "@/lib/apps";
+
+const mainCategories = [...new Set(apps.map(app => app.category))];
 
 const categoryGroups = {
-    "Games": ["Action", "Adventure", "Apple Arcade", "Board", "Card", "Casino", "Casual", "MMOG", "Music Game", "Paid", "Puzzle", "Racing", "Role Playing", "Simulation", "Sports", "Strategy", "Trivia", "Word"],
-    "Apps": ["Art & Design", "Beauty", "Books & Reference", "Business", "Comics", "Communication", "Dating", "Education", "Emulator Apps", "Entertainment", "Events", "Finance", "Health & Fitness", "House & Home", "IPA Apps", "jailbreak apps", "Lifestyle", "Maps & Navigation", "Media & Video", "Medical", "Music & Audio", "News And Magazines", "Personalization", "Photography", "Productivity", "Shopping", "Social", "Sport", "Tools", "Tools and apps", "Travel & Local", "Tweaked App Store", "Video Players", "Video Players & Editors", "Weather"]
+    "Categories": mainCategories,
 }
 
 export function Sidebar() {
@@ -15,7 +17,7 @@ export function Sidebar() {
                     <div className="flex items-center justify-between mb-4">
                         <h3 className="text-lg font-bold">{groupTitle}</h3>
                         <Button asChild variant="ghost" size="sm">
-                            <Link href={`/${groupTitle.toLowerCase()}`}>View More</Link>
+                            <Link href={`/apps`}>View All</Link>
                         </Button>
                     </div>
                     <div className="grid grid-cols-2 gap-x-4 gap-y-2">
