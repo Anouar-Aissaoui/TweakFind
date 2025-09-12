cbffce37903c2e3bdc960d0d408ef0ce28f2bd3d
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-12 17:54:43 +0000

    A sitemap.ts and robots.ts to guide search engines.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,8 +5,8 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/*?*'],
+      disallow: ['/*?q=*'],
     },
-    sitemap: 'https://tweak.appsg.site/sitemap.xml',
+sitemap: 'https://tweak.appsg.site/sitemap.xml',
   }
 }
