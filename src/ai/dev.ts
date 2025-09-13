cbcc004adc7f97ee6a60e6a98e937ebda6df4afb
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-13 16:11:03 +0000

    not work

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-// This import is not needed for the dev server and can cause build issues.
-// import '/src/app/api/genkit/[slug]/route';
-
-import '@/ai/flows/generate-app-description.ts';
-import '@/ai/flows/recommend-tweaked-apps.ts';
--- src/app/api/genkit/[slug]/route.ts
+++
@@ -1,7 +0,0 @@
-
-// src/app/api/genkit/[slug]/route.ts
-import { createGenkitNextAPIHandler } from '@genkit-ai/next';
-import '@/ai/flows/generate-app-description';
-import '@/ai/flows/recommend-tweaked-apps';
-
-export const { POST } = createGenkitNextAPIHandler();
