2f5ffa0a978ebfe2bf3a0acd55033a212a7f8b42
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-17 13:38:42 +0000

    pls you can add image url pls and chonge local image : https://i.imgur.c

--- next.config.ts
+++ next.config.ts
@@ -33,43 +33,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gyazo.com',
         port: '',
         pathname: '/**',
       },
