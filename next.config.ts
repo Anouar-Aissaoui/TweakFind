d4d1aca40b8347c1fd2291b27f305a17cf8af76b
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-17 15:58:46 +0000

    External Domain Configuration If your images are not working because the

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
